807452e9f0e5fa2018982515ddc8f78f43869161
Elga Ridlo <elgaridlo@Elgas-MacBook-Air.local>
2022-08-23 16:56:51 +0700

    Apply migration with fastify - Postgresql

--- src/app.js
+++ src/app.js
@@ -1,7 +1,12 @@
 const fastify = require('fastify');
+const db = require('../plugin/database')
 
 const build = (opts = {}) => {
   const app = fastify(opts);
+
+  // register plugins
+  app.register(db);
+
   app.get('/', (req, reply) => {
     reply.code(200).send({ hello: 'world!' });
   });
